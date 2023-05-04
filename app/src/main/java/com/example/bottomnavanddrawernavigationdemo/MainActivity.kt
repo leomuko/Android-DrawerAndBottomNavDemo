c40bda6701b4d0e5578e898a5ba3aa0a063f27b1
leomuko <kezzieleo@gmail.com>
2023-05-04 15:54:13 +0300

    Implement Drawer Layout

--- app/src/main/java/com/example/bottomnavanddrawernavigationdemo/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavanddrawernavigationdemo/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.bottomnavanddrawernavigationdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.bottomnavanddrawernavigationdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        /*binding.navView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            binding.drawerLayout.closeDrawers()
+
+            when (menuItem.itemId) {
+                R.id.firstFragment -> {
+                    navController.navigate(R.id.firstFragment)
+                    true
+                }
+                R.id.secondFragment -> {
+                    navController.navigate(R.id.secondFragment)
+                    true
+                }
+                else -> false
+            }
+        }*/
+
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
 }
